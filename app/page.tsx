c2c42a4424c77a44a468f41002f27e06d64bff52
Viktor <vikt.mag@gmail.com>
2023-06-10 21:38:19 +0200

    Cleaned up title-shadow and changed name to header-shadow-anim

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
         <h1 className="text-6xl font-extrabold">
-          <span className="title-shadow">Header</span>
+          <span className="header-shadow-anim">Header</span>
         </h1>
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
