fa517f44f562104a2fb86de88a8ab049efa99924
Viktor <vikt.mag@gmail.com>
2023-06-12 18:47:21 +0200

    Changed layout of LogoBanner

--- app/LogoBanner.tsx
+++ app/LogoBanner.tsx
@@ -14,58 +14,60 @@ import GitLogo from "./icons/GitLogo";
 
 export default function LogoBanner() {
   return (
-    <div className="flex w-full items-center justify-between">
-      <div>
-        <PythonLogo />
-        <p className="text-center font-bold py-1">Python</p>
-      </div>
-      <div>
-        <ReactLogo />
-        <p className="text-center font-bold py-1">React</p>
-      </div>
-      <div>
-        <JavaScriptLogo />
-        <p className="text-center font-bold py-1">JavaScript</p>
-      </div>
-      <div>
-        <TypeScriptLogo />
-        <p className="text-center font-bold py-1">TypeScript</p>
-      </div>
-      <div>
-        <HTMLLogo />
-        <p className="text-center font-bold py-1">HTML</p>
-      </div>
-      <div>
-        <CSSLogo />
-        <p className="text-center font-bold py-1">CSS</p>
-      </div>
-      <div>
-        <DjangoLogo />
-        <p className="text-center font-bold py-1">Django</p>
-      </div>
-      <div>
-        <NextJSLogo />
-        <p className="text-center font-bold py-1">Next.js</p>
-      </div>
-      <div>
-        <PostgresLogo />
-        <p className="text-center font-bold py-1">PostgreSQL</p>
-      </div>
-      <div>
-        <GraphQLLogo />
-        <p className="text-center font-bold py-1">GraphQL</p>
-      </div>
-      <div>
-        <TailwindLogo />
-        <p className="text-center font-bold py-1">Tailwind CSS</p>
-      </div>
-      <div>
-        <DockerLogo />
-        <p className="text-center font-bold py-1">Docker</p>
-      </div>
-      <div>
-        <GitLogo />
-        <p className="text-center font-bold py-1">Git</p>
+    <div className="mx-auto">
+      <div className="grid grid-cols-7 gap-4">
+        <div>
+          <PythonLogo />
+          <p className="text-center font-bold py-1">Python</p>
+        </div>
+        <div>
+          <ReactLogo />
+          <p className="text-center font-bold py-1">React</p>
+        </div>
+        <div>
+          <JavaScriptLogo />
+          <p className="text-center font-bold py-1">JavaScript</p>
+        </div>
+        <div>
+          <TypeScriptLogo />
+          <p className="text-center font-bold py-1">TypeScript</p>
+        </div>
+        <div>
+          <HTMLLogo />
+          <p className="text-center font-bold py-1">HTML</p>
+        </div>
+        <div>
+          <CSSLogo />
+          <p className="text-center font-bold py-1">CSS</p>
+        </div>
+        <div>
+          <DjangoLogo />
+          <p className="text-center font-bold py-1">Django</p>
+        </div>
+        <div>
+          <NextJSLogo />
+          <p className="text-center font-bold py-1">Next.js</p>
+        </div>
+        <div>
+          <PostgresLogo />
+          <p className="text-center font-bold py-1">PostgreSQL</p>
+        </div>
+        <div>
+          <GraphQLLogo />
+          <p className="text-center font-bold py-1">GraphQL</p>
+        </div>
+        <div>
+          <TailwindLogo />
+          <p className="text-center font-bold py-1">Tailwind CSS</p>
+        </div>
+        <div>
+          <DockerLogo />
+          <p className="text-center font-bold py-1">Docker</p>
+        </div>
+        <div>
+          <GitLogo />
+          <p className="text-center font-bold py-1">Git</p>
+        </div>
       </div>
     </div>
   );
