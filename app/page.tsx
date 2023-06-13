71aaf3fff61837f3e932ba908961275257e7c0e9
Viktor <vikt.mag@gmail.com>
2023-06-13 22:10:21 +0200

    Changed padding on main tag

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LogoBanner from "./LogoBanner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-10">
       <div className="">
         <h1 className="text-6xl font-extrabold">
           <span className="header-shadow-anim">Header</span>
