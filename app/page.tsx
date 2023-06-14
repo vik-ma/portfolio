072d9d2d839d5d416aa650304daea172a5931a89
Viktor <vikt.mag@gmail.com>
2023-06-14 06:14:54 +0200

    Changed text color of main

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LogoBanner from "./LogoBanner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-20 px-2 md:px-5">
+    <main className="flex min-h-screen flex-col items-center justify-between py-20 px-2 md:px-5 text-stone-100">
       <div className="">
         <h1 className="text-6xl font-extrabold">
           <span className="header-shadow-anim">Header</span>
