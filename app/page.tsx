661101e76c4823f38a040cde1d5e1a06e19a56cb
Viktor <vikt.mag@gmail.com>
2023-06-15 19:21:15 +0200

    Made Languages and Tools heading responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-20 px-2 md:px-5 text-stone-100">
       <div className="">
-        <h1 className="text-6xl font-extrabold">
+        <h1 className="text-7xl font-extrabold">
           <span className="header-shadow-anim">Header</span>
         </h1>
       </div>
       <div className="text-center">
-        <h2 className="pb-4 text-5xl font-extrabold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent">
+        <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent">
           Languages and Tools
         </h2>
         <div className="font-mono text-sm">
