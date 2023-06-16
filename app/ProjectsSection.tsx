274464d98258412f947165a45b67d26cf6572c04
Viktor <vikt.mag@gmail.com>
2023-06-16 23:02:48 +0200

    Made all projects in grid take up full width

--- app/ProjectsSection.tsx
+++ app/ProjectsSection.tsx
@@ -2,7 +2,7 @@ export default function ProjectsSection() {
   return (
     <div className="py-2 md:py-3 bg-neutral-700 border-white border-2">
       <div className="flex flex-wrap justify-center projects-container md:gap-x-2 md:gap-y-1 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 font-mono text-md text-amber-50">
+        <div className="grid grid-cols-1 w-full md:grid-cols-2 gap-3 font-mono text-md text-amber-50">
           <div className="p-2 bg-neutral-900">Col 1</div>
           <div className="p-2 bg-neutral-900">Col 2</div>
           <div className="p-2 bg-neutral-900">Col 1</div>
