23d431a7c9345241d28c5133c0869c0903709606
Viktor <vikt.mag@gmail.com>
2023-06-16 23:06:49 +0200

    Removed background for projects-container

--- app/ProjectsSection.tsx
+++ app/ProjectsSection.tsx
@@ -1,12 +1,12 @@
 export default function ProjectsSection() {
   return (
-    <div className="py-2 md:py-3 bg-neutral-700 border-white border-2">
+    <div className="py-2 md:py-3">
       <div className="flex flex-wrap justify-center projects-container md:gap-x-2 md:gap-y-1 mx-auto">
         <div className="grid grid-cols-1 w-full md:grid-cols-2 gap-3 font-mono text-md text-amber-50">
-          <div className="p-2 bg-neutral-900">Col 1</div>
-          <div className="p-2 bg-neutral-900">Col 2</div>
-          <div className="p-2 bg-neutral-900">Col 1</div>
-          <div className="p-2 bg-neutral-900">Col 2</div>
+          <div className="p-2 project-box">Col 1</div>
+          <div className="p-2 project-box">Col 2</div>
+          <div className="p-2 project-box">Col 1</div>
+          <div className="p-2 project-box">Col 2</div>
         </div>
       </div>
     </div>
