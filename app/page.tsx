89ed876a23fb462d0ed5e2b099d8bc2c4b54c320
Viktor <vikt.mag@gmail.com>
2023-06-16 21:56:32 +0200

    Added Projects header in various different colors

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,26 @@ export default function Home() {
           <LogoBanner />
         </div>
       </div>
+      {/* <div className="text-center">
+        <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-gradient-to-r from-blue-400 via-sky-300 to-cyan-300 bg-clip-text text-transparent">
+          Projects
+        </h2>
+      </div> */}
+      <div className="text-center">
+        <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-amber-900 via-amber-100 to-amber-900 bg-clip-text text-transparent">
+          Projects
+        </h2>
+      </div>
+      {/* <div className="text-center">
+        <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-neutral-700 via-neutral-100 to-neutral-700 bg-clip-text text-transparent">
+          Projects
+        </h2>
+      </div> */}
+      {/* <div className="text-center">
+        <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-blue-600 via-cyan-300 to-blue-600 bg-clip-text text-transparent">
+          Projects
+        </h2>
+      </div> */}
     </main>
   );
 }
