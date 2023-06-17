5a48d32aafb89a8669a8d15a653c20961c231dc0
Viktor <vikt.mag@gmail.com>
2023-06-17 08:10:54 +0200

    Moved background-gradient for project-box to unique id's

--- app/ProjectsSection.tsx
+++ app/ProjectsSection.tsx
@@ -2,9 +2,9 @@ export default function ProjectsSection() {
   return (
     <div className="py-2 md:py-3">
       <div className="flex flex-wrap justify-center projects-container md:gap-x-2 md:gap-y-1 mx-auto">
-        <div className="grid grid-cols-1 w-full md:grid-cols-2 gap-x-5 gap-y-10 font-mono text-md text-amber-50">
-          <div className="p-2 project-box">Col 1</div>
-          <div className="p-2 project-box">Col 2</div>
+        <div className="grid grid-cols-1 w-full md:grid-cols-2 gap-x-10 gap-y-10 font-mono text-md text-amber-50">
+          <div className="p-2 project-box" id="project-pps">Col 1</div>
+          <div className="p-2 project-box" id="project-pathfinding">Col 2</div>
           <div className="p-2 project-box">Col 1</div>
           <div className="p-2 project-box">Col 2</div>
         </div>
