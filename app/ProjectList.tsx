1fc646e87e794fb93f61d61c9eebd008eb408e22
Viktor <vikt.mag@gmail.com>
2023-06-19 15:00:43 +0200

    Created ProjectList and ProjectListProps type

---
+++ app/ProjectList.tsx
@@ -0,0 +1,32 @@
+export const ProjectList = [
+  {
+    id: "price-performance-scraper",
+    name: "Price-Performance Scraper",
+    description: "Price-Performance Scraper Description",
+  },
+  {
+    id: "pathfinding-race",
+    name: "Pathfinding Race",
+    description: "Pathfinding Race Description",
+  },
+  {
+    id: "paint-a-wave-function-collapse",
+    name: "Paint A Wave Function Collapse",
+    description: "Paint A Wave Function Collapse Description",
+  },
+  {
+    id: "screenshot-ocr",
+    name: "Screenshot OCR",
+    description: "Screenshot OCR Description",
+  },
+  {
+    id: "file-opener",
+    name: "File Opener",
+    description: "File Opener Description",
+  },
+  {
+    id: "link-opener",
+    name: "Link Opener",
+    description: "Link Opener Description",
+  },
+];
---
+++ typings.d.ts
@@ -0,0 +1,5 @@
+export type ProjectListProps = {
+    id: string;
+    name: string;
+    description: string;
+  };
