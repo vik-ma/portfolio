858a7ea938c05091d4063fb5e897a4403e9638d1
Viktor <vikt.mag@gmail.com>
2023-06-20 01:58:39 +0200

    Made techStack div stick to bottom of parent div

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -10,15 +10,23 @@ export default function ProjectsSection() {
         <div className="grid grid-cols-1 w-full md:grid-cols-2 py-4 px-6 gap-x-6 gap-y-10 font-sans text-md text-amber-50">
           {Object.entries(projects).map(([key, value]) => (
             <Link key={key} href={`/projects/${key}`}>
-              <div className="p-2 project-box" id={value.cssId}>
-                <div className="px-1 project-box-content">
-                  <h3 className="text-xl font-bold text-zinc-100">
-                    {value.name}
-                  </h3>
-                  <p className="project-short-desc">{value.shortDescription}</p>
+              <div className="p-2 h-full project-box" id={value.cssId}>
+                <div className="flex flex-col justify-between px-1 h-full project-box-content">
+                  <div>
+                    <h3 className="text-xl font-bold text-zinc-100">
+                      {value.name}
+                    </h3>
+                    <p className="project-short-desc">
+                      {value.shortDescription}
+                    </p>
+                  </div>
+
                   <div className="flex flex-wrap mb-0 -ml-1 text-stone-200">
                     {value.techStack.map((lang: string) => (
-                      <div key={`${key}-${lang}`} className="mr-1.5 mt-1 px-1.5 pb-0.5 font-semibold rounded-md border border-neutral-600">
+                      <div
+                        key={`${key}-${lang}`}
+                        className="mr-1.5 mt-1 px-1.5 pb-0.5 font-semibold rounded-md border border-neutral-600"
+                      >
                         {lang}
                       </div>
                     ))}
