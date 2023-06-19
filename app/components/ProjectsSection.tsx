b29ba44641ba13a0ef96cbd0c28323731ee42f54
Viktor <vikt.mag@gmail.com>
2023-06-20 01:20:06 +0200

    Added boxes for every language in techStack in ProjectsSection

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -7,7 +7,7 @@ export default function ProjectsSection() {
   return (
     <div className="py-2 md:py-3 md:mt-1 rounded-2xl projects-section-box">
       <div className="flex flex-wrap justify-center projects-container md:gap-x-2 md:gap-y-1 mx-auto">
-        <div className="grid grid-cols-1 w-full md:grid-cols-2 py-4 px-7 gap-x-8 gap-y-10 font-sans text-md text-amber-50">
+        <div className="grid grid-cols-1 w-full md:grid-cols-2 py-4 px-6 gap-x-6 gap-y-10 font-sans text-md text-amber-50">
           {Object.entries(projects).map(([key, value]) => (
             <Link key={key} href={`/projects/${key}`}>
               <div className="p-2 project-box" id={value.cssId}>
@@ -16,6 +16,13 @@ export default function ProjectsSection() {
                     {value.name}
                   </h3>
                   <p className="project-short-desc">{value.shortDescription}</p>
+                  <div className="flex flex-wrap mb-0 -ml-1 text-stone-200">
+                    {value.techStack.map((lang: string) => (
+                      <div key={`${key}-${lang}`} className="mr-1.5 mt-1 px-1.5 pb-0.5 font-semibold rounded-md border border-neutral-600">
+                        {lang}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </Link>
