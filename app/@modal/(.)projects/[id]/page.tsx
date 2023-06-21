7e88631807555697390d83d424dd401282eb3f6c
Viktor <vikt.mag@gmail.com>
2023-06-21 23:58:18 +0200

    Added CloseButton icon to Projects Modal

--- app/@modal/(.)projects/[id]/page.tsx
+++ app/@modal/(.)projects/[id]/page.tsx
@@ -2,6 +2,7 @@ import { ProjectsInfo } from "@/app/ProjectsInfo";
 import { ProjectsInfoProps } from "@/typings";
 import Modal from "@/app/components/Modal";
 import ProjectPage from "@/app/components/ProjectPage";
+import CloseButton from "@/app/icons/CloseButton";
 
 type PageProps = {
   params: {
@@ -15,11 +16,18 @@ export default function ProjectModal({ params: { id } }: PageProps) {
 
   return (
     <Modal>
-      <h1
-        className={`pb-2 text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent ${project?.cssGradient}`}
-      >
-        {project?.name}
-      </h1>
+      <div className="flex flex-row justify-between">
+        <h1
+          className={`pb-2 text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent ${project?.cssGradient}`}
+        >
+          {project?.name}
+        </h1>
+        <div className="flex items-baseline -mt-1 -mr-1">
+          <div className="p-1.5 rounded-lg hover:bg-neutral-700">
+            <CloseButton />
+          </div>
+        </div>
+      </div>
       <ProjectPage project={project} />
     </Modal>
   );
