395b343569feec90f597d29f701c6a58f8d5666a
Viktor <vikt.mag@gmail.com>
2023-06-26 23:46:08 +0200

    Added currentImageIndex useState to ImageSlider

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -1,28 +1,41 @@
 "use client";
 import { ImageSliderProps } from "@/typings";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ImageSlider({
   name,
   previewImgSrcList,
   previewImgBase64DataList,
   previewImgWidthList,
-  previewImgHeightList
+  previewImgHeightList,
 }: ImageSliderProps) {
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
   return (
-    <div className="flex">
-      {previewImgSrcList?.map((imageSrc: string, index: number) => (
-        <Image
-          className="max-w-[50px] md:max-w-none border border-neutral-700 ml-0.5"
-          key={index + 1}
-          src={imageSrc}
-          alt={`${name} Preview Image ${index + 1}`}
-          width={previewImgWidthList[index]}
-          height={previewImgHeightList[index]}
-          placeholder="blur"
-          blurDataURL={previewImgBase64DataList[index]}
-        />
-      ))}
+    <div>
+      <Image
+        className="mb-1"
+        src={previewImgSrcList[currentImageIndex]}
+        alt={`${name} Preview Image ${currentImageIndex + 1}`}
+        width={300}
+        height={200}
+      />
+      <div className="flex justify-center">
+        {previewImgSrcList?.map((imageSrc: string, index: number) => (
+          <Image
+            className="max-w-[50px] md:max-w-none border border-neutral-700 mr-0.5"
+            key={index + 1}
+            src={imageSrc}
+            alt={`${name} Preview Image ${index + 1}`}
+            width={previewImgWidthList[index]}
+            height={previewImgHeightList[index]}
+            placeholder="blur"
+            blurDataURL={previewImgBase64DataList[index]}
+            onClick={() => setCurrentImageIndex(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
