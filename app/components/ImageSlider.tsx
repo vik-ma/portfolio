a2b527f17d781b0c1498bd5cdd3e0e654ebf4643
Viktor <vikt.mag@gmail.com>
2023-06-28 03:36:19 +0200

    Added opacity features to preview thumbnails

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -61,7 +61,7 @@ export default function ImageSlider({
         <div className="flex justify-center mt-1.5">
           {previewImgSrcList?.map((imageSrc: string, index: number) => (
             <Image
-              className="max-w-[50px] max-[870px]:max-w-none border border-neutral-700 hover:border-neutral-400 mx-[1px] cursor-pointer"
+              className="max-w-[50px] max-[870px]:max-w-none border border-neutral-700 hover:border-neutral-300 mx-[1px] cursor-pointer opacity-70 hover:opacity-100"
               key={index + 1}
               src={imageSrc}
               alt={`${name} Preview Image ${index + 1}`}
