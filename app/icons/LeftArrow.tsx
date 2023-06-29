392c2fac83f411eca025dc6e76f0b1ae9700397f
Viktor <vikt.mag@gmail.com>
2023-06-29 22:37:25 +0200

    Added RightArrow and LeftArrow SVG components

---
+++ app/icons/LeftArrow.tsx
@@ -0,0 +1,16 @@
+export default function LeftArrow() {
+  return (
+    <svg
+      width="32px"
+      height="32px"
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#bef264"
+      stroke="#bef264"
+      strokeWidth="102.4"
+    >
+      <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z"></path>
+    </svg>
+  );
+}
