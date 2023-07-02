528c720cafc35497035614a178d63b931c9f0a61
Viktor <vikt.mag@gmail.com>
2023-07-02 09:36:18 +0200

    Changed name of LogoBanner component to LanguagesSection and tweaked its structure slightly

---
+++ app/components/LanguagesSection.tsx
@@ -0,0 +1,133 @@
+import PythonLogo from "../icons/PythonLogo";
+import ReactLogo from "../icons/ReactLogo";
+import JavaScriptLogo from "../icons/JavaScriptLogo";
+import TypeScriptLogo from "../icons/TypeScriptLogo";
+import HTMLLogo from "../icons/HTMLLogo";
+import CSSLogo from "../icons/CSSLogo";
+import DjangoLogo from "../icons/DjangoLogo";
+import NextJSLogo from "../icons/NextJSLogo";
+import PostgresLogo from "../icons/PostgresLogo";
+import GraphQLLogo from "../icons/GraphQLLogo";
+import TailwindLogo from "../icons/TailwindLogo";
+import DockerLogo from "../icons/DockerLogo";
+import GitLogo from "../icons/GitLogo";
+
+export default function LanguagesSection() {
+  return (
+    <div className="text-center">
+      <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent">
+        Languages and Tools
+      </h2>
+      <div className="rounded-3xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1.5">
+        <div className="py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box">
+          <div className="flex flex-wrap justify-center lang-list-container md:gap-x-2 md:gap-y-1 mx-auto">
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <PythonLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                Python
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <ReactLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                React
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <JavaScriptLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                JavaScript
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <TypeScriptLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                TypeScript
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <HTMLLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                HTML
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <CSSLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                CSS
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <TailwindLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                Tailwind
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <DjangoLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                Django
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <NextJSLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                Next.js
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <PostgresLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                PostgreSQL
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <GraphQLLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                GraphQL
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <DockerLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                Docker
+              </p>
+            </div>
+            <div className="text-center items-center p-3 logo-wrapper">
+              <div className="logo-container m-auto">
+                <GitLogo />
+              </div>
+              <p className="font-semibold py-1 text-xs md:text-sm mt-0.5">
+                Git
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import Image from "next/image";
-import LogoBanner from "./components/LogoBanner";
+import LanguagesSection from "./components/LanguagesSection";
 import ProjectsSection from "./components/ProjectsSection";
 import HeroSection from "./components/HeroSection";
 
-
 export default function Home() {
-
   return (
-
     <main className="flex min-h-screen flex-col items-center justify-between py-20 px-2 md:px-5">
       <header className="py-7">
-      {/* <HeroSection /> */}
+        {/* <HeroSection /> */}
         <div>
           <h1 className="text-7xl font-extrabold">
             <span className="header-shadow-anim">Header</span>
@@ -18,12 +15,7 @@ export default function Home() {
         </div>
       </header>
       <section className="mt-20 py-7" id="languages">
-        <div className="text-center">
-          <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent">
-            Languages and Tools
-          </h2>
-          <LogoBanner />
-        </div>
+        <LanguagesSection />
       </section>
       {/* <div className="text-center">
         <h2 className="pb-4 text-4xl md:text-6xl font-extrabold bg-gradient-to-r from-blue-400 via-sky-300 to-cyan-300 bg-clip-text text-transparent">
@@ -49,5 +41,5 @@ export default function Home() {
         <ProjectsSection />
       </section>
     </main>
-    );
+  );
 }
