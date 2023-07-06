4b09e9a8a021e034dba511883b3830bff312b99c
Viktor <vikt.mag@gmail.com>
2023-07-06 04:47:55 +0200

    Changed gradient for LanguagesSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 export default function HeroSection() {
   return (
-    <div className="relative">
+    <div className="relative text-center">
       <h1 className="text-7xl font-extrabold">
         <span className="header-shadow-anim">Header</span>
       </h1>
@@ -27,6 +27,9 @@ export default function HeroSection() {
           Y
         </span>
       </div> */}
+      <div className="mt-[200px] rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5">
+        <div className="py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box w-[850px] h-[200px]"></div>
+      </div>
     </div>
   );
 }
