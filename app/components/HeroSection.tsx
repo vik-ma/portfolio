a71b0759a7e5a14f7ebcc5dc54bb726b933c525a
Viktor <vikt.mag@gmail.com>
2023-07-06 05:00:42 +0200

    Added smooth scrolling to CSS

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function HeroSection() {
   return (
     <div className="relative text-center">
@@ -28,7 +30,9 @@ export default function HeroSection() {
         </span>
       </div> */}
       <div className="mt-[200px] rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5">
-        <div className="py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box w-[850px] h-[200px]"></div>
+        <div className="py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box w-[850px] h-[200px]">
+          <Link href={"/#projects"}>Go To Projects</Link>
+        </div>
       </div>
     </div>
   );
