8ae1579b3e7eeb9fd78951254d48e405ff144fa6
Viktor <vikt.mag@gmail.com>
2023-07-08 01:23:08 +0200

    Added languages link to HeroSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -31,6 +31,8 @@ export default function HeroSection() {
       </div> */}
       <div className="mt-[200px] rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5">
         <div className="py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box w-[850px] h-[200px]">
+          <Link href={"/#languages"}>Go To Languages</Link>
+          <br />
           <Link href={"/#projects"}>Go To Projects</Link>
         </div>
       </div>
