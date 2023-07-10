b0689ecd513e8ed1b289b8fc0bc0c43acfb3e088
Viktor <vikt.mag@gmail.com>
2023-07-10 22:22:16 +0200

    Added function to add or remove header-shadow-anim class from Header

--- app/components/HeaderAnimation.tsx
+++ app/components/HeaderAnimation.tsx
@@ -1,15 +1,35 @@
+"use client";
+import { useRef } from "react";
+
 export default function HeaderAnimation() {
+  const headerRef = useRef<HTMLHeadingElement>(null);
+
+  const toggleAnimCheckbox = (checked: boolean) => {
+    if (checked) {
+      headerRef.current?.classList.remove("header-shadow-anim");
+    } else {
+      headerRef.current?.classList.add("header-shadow-anim");
+    }
+  };
   return (
     <div className="flex flex-col">
       <div>
         <h1 className="text-7xl font-extrabold">
-          <span className="header-shadow-anim">Header Headertwoo</span>
+          <span ref={headerRef} className="header-shadow-anim text-white">
+            Header Headertwoo
+          </span>
         </h1>
       </div>
       <div className="mt-12 relative">
-        <label className="absolute right-5 text-neutral-100 font-semibold">
-          <input type="checkbox" className="accent-neutral-800 mr-1.5" />
-          Disable Animation
+        <label className="inline-flex items-center absolute right-5 cursor-pointer">
+          <input
+            type="checkbox"
+            className="h-4 w-4 accent-neutral-100 text-gray-900"
+            onChange={(event) => toggleAnimCheckbox(event.target.checked)}
+          />
+          <span className="text-neutral-100 font-semibold ml-1.5">
+            Disable Animation
+          </span>
         </label>
       </div>
     </div>
