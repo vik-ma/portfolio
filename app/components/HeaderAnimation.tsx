f6603bffc92f96a479fa3bb79bbd536aea491c2f
Viktor <vikt.mag@gmail.com>
2023-07-10 21:01:17 +0200

    Added checkbox to disable Header animation

--- app/components/HeaderAnimation.tsx
+++ app/components/HeaderAnimation.tsx
@@ -1,9 +1,17 @@
 export default function HeaderAnimation() {
   return (
-    <div>
-      <h1 className="text-7xl font-extrabold">
-        <span className="header-shadow-anim">Header Headertwoo</span>
-      </h1>
+    <div className="flex flex-col">
+      <div>
+        <h1 className="text-7xl font-extrabold">
+          <span className="header-shadow-anim">Header Headertwoo</span>
+        </h1>
+      </div>
+      <div className="mt-12 relative">
+        <label className="absolute right-5 text-neutral-100 font-semibold">
+          <input type="checkbox" className="accent-neutral-800 mr-1.5" />
+          Disable Animation
+        </label>
+      </div>
     </div>
   );
 }
