27fee20ae909fba96216d8f3331416265726aced
Viktor <vikt.mag@gmail.com>
2023-07-10 22:56:20 +0200

    Changed opacity of animation checkbox label

--- app/components/HeaderAnimation.tsx
+++ app/components/HeaderAnimation.tsx
@@ -14,19 +14,22 @@ export default function HeaderAnimation() {
     }
   };
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col group">
       <div>
         <h1 className="text-7xl font-extrabold">
-          <span ref={headerRef} className="header-shadow header-shadow-anim text-white">
+          <span
+            ref={headerRef}
+            className="header-shadow header-shadow-anim text-white"
+          >
             Header Headertwoo
           </span>
         </h1>
       </div>
-      <div className="mt-12 relative">
-        <label className="inline-flex items-center absolute right-5 cursor-pointer">
+      <div className="group mt-12 relative">
+        <label className="inline-flex items-center absolute right-5 cursor-pointer opacity-0 group-hover:opacity-80 transition duration-200 ease-out">
           <input
             type="checkbox"
-            className="h-4 w-4 accent-neutral-100 text-gray-900"
+            className="h-4 w-4 accent-neutral-100 text-gray-900 "
             onChange={(event) => toggleAnimCheckbox(event.target.checked)}
           />
           <span className="text-neutral-100 font-semibold ml-1.5">
