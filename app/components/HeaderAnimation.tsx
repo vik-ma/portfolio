d412bc6543e4d969c7ac4637a28bf62cba3566c2
Viktor <vikt.mag@gmail.com>
2023-07-11 22:23:06 +0200

    Added text to disable animation for smaller screens

--- app/components/HeaderAnimation.tsx
+++ app/components/HeaderAnimation.tsx
@@ -10,7 +10,6 @@ export default function HeaderAnimation() {
     if (!disableAnimation) {
       headerTextRef.current?.classList.remove("header-shadow-anim");
       headerTextRef.current?.classList.add("header-shadow-static");
-
     } else {
       headerTextRef.current?.classList.remove("header-shadow-static");
       headerTextRef.current?.classList.add("header-shadow-anim");
@@ -40,11 +39,16 @@ export default function HeaderAnimation() {
         </h1>
       </div>
       <div className="mt-12 relative">
-        <label className="inline-flex items-center absolute right-5 opacity-0 group-hover:opacity-80 transition duration-300 ease-out -z-[1]"
-        ref={headerCheckRef}>
+        <span className="font-semibold italic text-neutral-100 opacity-50 select-none md:hidden">
+          Click to disable animation
+        </span>
+        <label
+          className="inline-flex items-center absolute right-5 opacity-0 group-hover:opacity-80 transition duration-300 ease-out -z-[1]"
+          ref={headerCheckRef}
+        >
           <input
             type="checkbox"
-            className="h-4 w-4 accent-neutral-100 text-gray-900"
+            className="h-4 w-4 accent-neutral-100"
             checked={disableAnimation}
             readOnly={true}
           />
