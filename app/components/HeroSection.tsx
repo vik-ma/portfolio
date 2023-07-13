771a2ad619c633d9422e6d0284bd771d0b200ebc
Viktor <vikt.mag@gmail.com>
2023-07-13 04:06:45 +0200

    Fixed positioning of Hero div

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -5,11 +5,11 @@ export default function HeroSection() {
   return (
     <div className="relative">
       <HeaderAnimation />
-      <div className="mt-[200px] rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5">
-        <div className="py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box w-[850px] h-[200px]">
-          <Link href={"/#languages"}>Go To Languages</Link>
+      <div className="mt-20 rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5">
+        <div className="text-center py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box w-[850px] h-[200px]">
+          <Link className="text-3xl" href={"/#languages"}>View Languages</Link>
           <br />
-          <Link href={"/#projects"}>Go To Projects</Link>
+          <Link className="text-3xl" href={"/#projects"}>View Projects</Link>
         </div>
       </div>
     </div>
