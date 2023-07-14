331ec78d9e7abd007968fc0d431c86be75c57da1
Viktor <vikt.mag@gmail.com>
2023-07-14 22:21:19 +0200

    Removed redundant span element

--- app/components/HeaderAnimation.tsx
+++ app/components/HeaderAnimation.tsx
@@ -33,55 +33,51 @@ export default function HeaderAnimation() {
         ref={headerTextRef}
       ></div>
       <div>
-        <h1 className="text-[75px] font-extrabold">
-          <span className="select-none">
-            <span className="header-text-container flex flex-row justify-center w-full">
-              <span className="flex flex-row justify-center">
-                <span className="letter letter-f-1">V</span>
-                <span className="letter letter-f-2">I</span>
-                <span className="letter letter-f-3">K</span>
-                <span className="letter letter-f-4">T</span>
-                <span className="letter letter-f-5">O</span>
-                <span className="letter letter-f-6">R</span>
-              </span>
-              <span className="flex flex-row justify-center ml-7">
-                <span className="letter letter-l-1">M</span>
-                <span className="letter letter-l-2">A</span>
-                <span className="letter letter-l-3">G</span>
-                <span className="letter letter-l-4">N</span>
-                <span className="letter letter-l-5">U</span>
-                <span className="letter letter-l-6">S</span>
-                <span className="letter letter-l-7">S</span>
-                <span className="letter letter-l-8">O</span>
-                <span className="letter letter-l-9">N</span>
-              </span>
+        <h1 className="text-[75px] font-extrabold select-none">
+          <span className="header-text-container flex flex-row justify-center w-full">
+            <span className="flex flex-row justify-center">
+              <span className="letter letter-f-1">V</span>
+              <span className="letter letter-f-2">I</span>
+              <span className="letter letter-f-3">K</span>
+              <span className="letter letter-f-4">T</span>
+              <span className="letter letter-f-5">O</span>
+              <span className="letter letter-f-6">R</span>
+            </span>
+            <span className="flex flex-row justify-center ml-7">
+              <span className="letter letter-l-1">M</span>
+              <span className="letter letter-l-2">A</span>
+              <span className="letter letter-l-3">G</span>
+              <span className="letter letter-l-4">N</span>
+              <span className="letter letter-l-5">U</span>
+              <span className="letter letter-l-6">S</span>
+              <span className="letter letter-l-7">S</span>
+              <span className="letter letter-l-8">O</span>
+              <span className="letter letter-l-9">N</span>
             </span>
           </span>
         </h1>
       </div>
       <div className="mt-10">
-        <h1 className="text-[75px] font-extrabold">
-          <span className="select-none">
-            <span className="header-text-container flex flex-row justify-center w-full mt-20">
-              <span className="flex flex-row justify-center ">
-                <span className="letter letter-f-12">V</span>
-                <span className="letter letter-f-22">I</span>
-                <span className="letter letter-f-32">K</span>
-                <span className="letter letter-f-42">T</span>
-                <span className="letter letter-f-52">O</span>
-                <span className="letter letter-f-62">R</span>
-              </span>
-              <span className="flex flex-row justify-center ml-7">
-                <span className="letter letter-l-12">M</span>
-                <span className="letter letter-l-22">A</span>
-                <span className="letter letter-l-32">G</span>
-                <span className="letter letter-l-42">N</span>
-                <span className="letter letter-l-52">U</span>
-                <span className="letter letter-l-62">S</span>
-                <span className="letter letter-l-72">S</span>
-                <span className="letter letter-l-82">O</span>
-                <span className="letter letter-l-92">N</span>
-              </span>
+        <h1 className="text-[75px] font-extrabold select-none">
+          <span className="header-text-container flex flex-row justify-center w-full mt-20">
+            <span className="flex flex-row justify-center">
+              <span className="letter letter-f-12">V</span>
+              <span className="letter letter-f-22">I</span>
+              <span className="letter letter-f-32">K</span>
+              <span className="letter letter-f-42">T</span>
+              <span className="letter letter-f-52">O</span>
+              <span className="letter letter-f-62">R</span>
+            </span>
+            <span className="flex flex-row justify-center ml-7">
+              <span className="letter letter-l-12">M</span>
+              <span className="letter letter-l-22">A</span>
+              <span className="letter letter-l-32">G</span>
+              <span className="letter letter-l-42">N</span>
+              <span className="letter letter-l-52">U</span>
+              <span className="letter letter-l-62">S</span>
+              <span className="letter letter-l-72">S</span>
+              <span className="letter letter-l-82">O</span>
+              <span className="letter letter-l-92">N</span>
             </span>
           </span>
         </h1>
