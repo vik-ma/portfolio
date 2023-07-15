28b8a39ce67afd3b8b74056331d8bc529d9ef951
Viktor <vikt.mag@gmail.com>
2023-07-15 03:20:00 +0200

    Adder header-wrapper and hero-container classes to contain header section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,15 +3,15 @@ import HeaderAnimation from "./HeaderAnimation";
 
 export default function HeroSection() {
   return (
-    <div className="relative">
+    <div className="relative header-wrapper">
       <HeaderAnimation />
-      <div className="mt-20 rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5">
-        <div className="text-center py-2 md:py-3 font-sans text-sm text-zinc-100 langs-section-box w-[850px] h-[200px]">
+      {/* <div className="mt-20 rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5"> */}
+        <div className="hero-container h-[200px] mx-1 text-center py-2 md:py-3 font-sans text-sm text-zinc-100">
           <Link className="text-3xl" href={"/#languages"}>View Languages</Link>
           <br />
           <Link className="text-3xl" href={"/#projects"}>View Projects</Link>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 }
