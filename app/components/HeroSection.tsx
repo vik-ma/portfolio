13569bc9c2090c05d1fe5eb85c6ec4e52dc5cc73
Viktor <vikt.mag@gmail.com>
2023-07-15 03:26:06 +0200

    Made hero-container responsive

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
     <div className="relative header-wrapper">
       <HeaderAnimation />
       {/* <div className="mt-20 rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5"> */}
-        <div className="hero-container h-[200px] mx-1 mt-20 text-center py-2 md:py-3 font-sans text-sm text-zinc-100">
+        <div className="hero-container h-[200px] mx-auto mt-20 text-center py-2 md:py-3 font-sans text-sm text-zinc-100">
           <Link className="text-3xl" href={"/#languages"}>View Languages</Link>
           <br />
           <Link className="text-3xl" href={"/#projects"}>View Projects</Link>
