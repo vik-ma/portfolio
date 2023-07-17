e034b18e9297a7eeb47f3dbd6bdbace2c50e9552
Viktor <vikt.mag@gmail.com>
2023-07-17 22:25:09 +0200

    Added hero-inner-container class and changed the border of hero-container

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -6,25 +6,25 @@ export default function HeroSection() {
     <div className="relative header-wrapper flex flex-row justify-center">
       <div className="flex flex-col justify-center">
         <HeaderAnimation />
-        {/* <div className="mt-20 rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5"> */}
-        <div className="hero-container h-[200px] mx-auto mt-20 text-center py-2 md:py-3 font-sans text-sm text-zinc-100">
-          <div className="flex flex-col justify-between p-2 h-full">
-            <div></div>
-            <div className="flex flex-row justify-evenly">
-              <Link className="font-bold text-3xl" href={"/#languages"}>
-                View Languages
-              </Link>
-              <Link className="font-bold text-3xl" href={"/#projects"}>
-                View Projects
-              </Link>
-              <Link className="font-bold text-3xl" href={"/about"}>
-                About
-              </Link>
+        <div className="hero-container mt-20 rounded-3xl bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1.5 p-1.5">
+          <div className="hero-inner-container h-[200px] mx-auto text-center py-2 md:py-3 font-sans text-sm text-zinc-100">
+            <div className="flex flex-col justify-between p-2 h-full">
+              <div></div>
+              <div className="flex flex-row justify-evenly">
+                <Link className="font-bold text-3xl" href={"/#languages"}>
+                  View Languages
+                </Link>
+                <Link className="font-bold text-3xl" href={"/#projects"}>
+                  View Projects
+                </Link>
+                <Link className="font-bold text-3xl" href={"/about"}>
+                  About
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      {/* </div> */}
     </div>
   );
 }
