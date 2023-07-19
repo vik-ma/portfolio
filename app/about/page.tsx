2a539fab42d2377c020dcd5d011181175458d6c5
Viktor <vikt.mag@gmail.com>
2023-07-19 07:01:04 +0200

    Added BackArrow to Back button in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import BackArrow from "../icons/BackArrow";
 
 export default function About() {
   return (
     <div className="flex h-screen justify-center items-center">
       <div>
         <Link href="/">
-          <button className="mb-5 py-2.5 px-4 rounded-xl font-bold text-lg text-zinc-100 bg-neutral-800 hover:bg-neutral-700">
-            Back To Homepage
+          <button className="single-page-back-button mb-5 py-2.5 px-4 rounded-xl font-bold text-xl text-stone-100 hover:bg-stone-700">
+            <span className="inline-block align-middle">
+              <BackArrow />
+            </span>
+            <span className="inline-block align-middle ml-2.5">
+              Back To Homepage
+            </span>
           </button>
         </Link>
         <div className="px-4 py-3 md:py-4 rounded-xl max-md:text-center project-page-container">
