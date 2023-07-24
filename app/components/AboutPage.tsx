fe04785162a673d61c1caa066356b5cfddd59e5d
Viktor <vikt.mag@gmail.com>
2023-07-24 23:52:52 +0200

    Added Credits h2 tag to AboutPage

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -1,11 +1,14 @@
 export default function AboutPage() {
   return (
-    <div>
-      <h1
-        className={`w-fit pb-0.5 md:pb-2 text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400`}
-      >
+    <div className="text-stone-200">
+      <h1 className="w-fit pb-0.5 md:pb-2 text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         About
       </h1>
+      <p>ASD</p>
+      <h2 className="w-fit pb-0.5 md:pb-2 text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
+        Credits
+      </h2>
+      <p>ASDASD</p>
     </div>
   );
 }
