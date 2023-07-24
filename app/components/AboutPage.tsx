3dd7eebbf424f138aff90b81264bd7661a57d5c4
Viktor <vikt.mag@gmail.com>
2023-07-24 23:57:41 +0200

    Modified paddings for AboutPage

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -1,14 +1,14 @@
 export default function AboutPage() {
   return (
     <div className="text-stone-200">
-      <h1 className="w-fit pb-0.5 md:pb-2 text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
+      <h1 className="w-fit text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         About
       </h1>
-      <p>ASD</p>
-      <h2 className="w-fit pb-0.5 md:pb-2 text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
+      <p className="p-1 pb-2">ASD</p>
+      <h2 className="w-fit text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         Credits
       </h2>
-      <p>ASDASD</p>
+      <p className="p-1">ASDASD</p>
     </div>
   );
 }
