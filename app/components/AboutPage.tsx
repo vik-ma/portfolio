7902d7b461420464572556dcc9ba6779d9604e82
Viktor <vikt.mag@gmail.com>
2023-07-25 00:31:26 +0200

    Added text to the About section

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -4,7 +4,13 @@ export default function AboutPage() {
       <h1 className="w-fit text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         About
       </h1>
-      <p className="p-1 pb-2">ASD</p>
+      <p className="p-1 pb-2">
+        This website was designed by me and made using{" "}
+        <strong className="text-stone-100">React</strong>,{" "}
+        <strong className="text-stone-100">TypeScript</strong>,{" "}
+        <strong className="text-stone-100">Next.js</strong> and{" "}
+        <strong className="text-stone-100">TailwindCSS</strong>.
+      </p>
       <h2 className="w-fit text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         Credits
       </h2>
