509733f4d5d395d654587a333f2356a89ce237ab
Viktor <vikt.mag@gmail.com>
2023-07-25 00:36:45 +0200

    Aligned About text left and modified the padding for smaller screens

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -1,10 +1,10 @@
 export default function AboutPage() {
   return (
-    <div className="text-stone-200">
+    <div className="text-stone-200 text-left">
       <h1 className="w-fit text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         About
       </h1>
-      <p className="p-1 pb-2">
+      <p className="p-0.5 md:p-1 pb-2">
         This website was designed by me and made using{" "}
         <strong className="text-stone-100">React</strong>,{" "}
         <strong className="text-stone-100">TypeScript</strong>,{" "}
@@ -14,7 +14,7 @@ export default function AboutPage() {
       <h2 className="w-fit text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         Credits
       </h2>
-      <p className="p-1">ASDASD</p>
+      <p className="p-0.5 md:p-1">ASDASD</p>
     </div>
   );
 }
