885e5ead2616ec6480405ca9cfb4efa37e6afe07
Viktor <vikt.mag@gmail.com>
2023-07-24 23:33:41 +0200

    Added gradient to About title

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <div>
       <h1
-        className={`pb-0.5 md:pb-2 text-3xl md:text-5xl font-extrabold text-neutral-100`}
+        className={`w-fit pb-0.5 md:pb-2 text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-amber-500 to-yellow-400`}
       >
         About
       </h1>
