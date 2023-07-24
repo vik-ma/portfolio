42bc879c3412914a21a234e6baa18e6eefcd5643
Viktor <vikt.mag@gmail.com>
2023-07-25 01:51:27 +0200

    Added credits to AboutPage

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -4,17 +4,46 @@ export default function AboutPage() {
       <h1 className="w-fit text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         About
       </h1>
-      <p className="p-0.5 md:p-1 pb-2">
+      <p className="p-0.5 pt-0 md:p-1">
         This website was designed by me and made using{" "}
         <strong className="text-stone-100">React</strong>,{" "}
         <strong className="text-stone-100">TypeScript</strong>,{" "}
         <strong className="text-stone-100">Next.js</strong> and{" "}
         <strong className="text-stone-100">TailwindCSS</strong>.
       </p>
-      <h2 className="w-fit text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
+      <h2 className="w-fit mt-2 text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         Credits
       </h2>
-      <p className="p-0.5 md:p-1">ASDASD</p>
+      <ul className="p-0.5 md:p-1 text-sm">
+        <li>
+          Icons for programming tools and languages from{" "}
+          <a
+            className="font-semibold text-blue-400 hover:underline"
+            href="https://devicon.dev/"
+            target="_blank"
+          >
+            Devicon
+          </a>
+        </li>
+        <li>
+          Modified GBA startup CSS animation originally made by{" "}
+          <a
+            className="font-semibold text-blue-400 hover:underline"
+            href="https://codepen.io/rvervuurt/pen/bdZQrE"
+            target="_blank"
+          >
+            rvervuurt
+          </a>
+        </li>
+        <li>
+          Shadow gradients from{" "}
+          <a
+            className="font-semibold text-blue-400 hover:underline"
+            href="https://alvarotrigo.com/shadow-gradients/"
+            target="_blank"
+          >alvarotrigo</a>
+        </li>
+      </ul>
     </div>
   );
 }
