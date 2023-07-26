7526334a65fd268c4bfb6f40c007166bd924a828
Viktor <vikt.mag@gmail.com>
2023-07-26 15:40:32 +0200

    Tweaked margins for AboutPage

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -10,13 +10,13 @@ export default function AboutPage() {
         About
       </h1>
       <p className="p-0.5 pt-0 md:p-1">
-        This website was designed by me and made using{" "}
+        This website was designed by me, <strong className="text-stone-100">Viktor Magnusson</strong>, and made using{" "}
         <strong className="text-stone-100">React</strong>,{" "}
         <strong className="text-stone-100">TypeScript</strong>,{" "}
         <strong className="text-stone-100">Next.js 13</strong> and{" "}
         <strong className="text-stone-100">TailwindCSS</strong>.
       </p>
-      <div className="flex justify-center gap-x-4 sm:gap-x-6 mt-2 overflow-hidden">
+      <div className="flex justify-center gap-x-4 sm:gap-x-6 mt-1.5 sm:mt-2.5 overflow-hidden">
         <div id="logo-1" className="max-w-[50px] sm:max-w-[64px] w-full about-logo-anim-1">
           <ReactLogo />
         </div>
@@ -34,7 +34,7 @@ export default function AboutPage() {
         Credits
       </h2>
       <ul className="p-0.5 md:p-1 text-sm text-stone-300">
-        <li className="max-sm:mb-1.5">
+        <li className="max-sm:mb-1 mb-0.5">
           Icons for programming tools and languages from{" "}
           <a
             className="font-semibold text-blue-400 hover:underline"
@@ -44,7 +44,7 @@ export default function AboutPage() {
             Devicon
           </a>
         </li>
-        <li className="max-sm:mb-1.5">
+        <li className="max-sm:mb-1 mb-0.5">
           Modified GBA startup CSS animation originally made by{" "}
           <a
             className="font-semibold text-blue-400 hover:underline"
