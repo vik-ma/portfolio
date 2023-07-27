1f87ba8f7842bec453b917d447c9f8776156f8a6
Viktor <vikt.mag@gmail.com>
2023-07-27 23:56:59 +0200

    Added titles to not-found pages

--- app/projects/[id]/not-found.tsx
+++ app/projects/[id]/not-found.tsx
@@ -3,15 +3,18 @@ import Link from "next/link";
 export default function NotFound() {
   // Display this page if user types in Project ID that does not exist
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="text-neutral-100 font-extrabold text-2xl md:text-6xl pb-3 md:pb-5">
-        Project Does Not Exist!
-      </h1>
-      <Link href="/#projects">
-        <button className="not-found-page-back-button py-1.5 px-3.5 md:py-2.5 md:px-4 rounded-xl font-bold text-base md:text-xl text-zinc-100 bg-neutral-800 hover:bg-stone-700 border-2 border-neutral-100">
-          Back To Projects
-        </button>
-      </Link>
-    </div>
+    <>
+      <title>Project Does Not Exist</title>
+      <div className="flex flex-col h-screen justify-center items-center">
+        <h1 className="text-neutral-100 font-extrabold text-2xl md:text-6xl pb-3 md:pb-5">
+          Project Does Not Exist!
+        </h1>
+        <Link href="/#projects">
+          <button className="not-found-page-back-button py-1.5 px-3.5 md:py-2.5 md:px-4 rounded-xl font-bold text-base md:text-xl text-zinc-100 bg-neutral-800 hover:bg-stone-700 border-2 border-neutral-100">
+            Back To Projects
+          </button>
+        </Link>
+      </div>
+    </>
   );
 }
