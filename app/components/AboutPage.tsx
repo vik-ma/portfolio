f5e0c17e6b9c00fe4e064d6fd1398764babe7a6e
Viktor <vikt.mag@gmail.com>
2023-07-27 20:49:06 +0200

    Tweaked margins in AboutPage

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -9,14 +9,14 @@ export default function AboutPage() {
       <h1 className="w-fit text-3xl md:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         About
       </h1>
-      <p className="p-0.5 pt-0 md:p-1">
+      <p className="max-sm:mt-1 p-0.5 pt-0 md:p-1">
         This website was designed by me, <strong className="text-stone-100">Viktor Magnusson</strong>, and made using{" "}
         <strong className="text-stone-100">React</strong>,{" "}
         <strong className="text-stone-100">TypeScript</strong>,{" "}
         <strong className="text-stone-100">Next.js 13</strong> and{" "}
         <strong className="text-stone-100">Tailwind CSS</strong>.
       </p>
-      <div className="flex justify-center gap-x-4 sm:gap-x-6 mt-1.5 sm:mt-2.5 overflow-hidden">
+      <div className="flex justify-center gap-x-4 sm:gap-x-6 mt-4 sm:mt-2.5 overflow-hidden">
         <div id="logo-1" className="max-w-[50px] sm:max-w-[64px] w-full about-logo-anim-1">
           <ReactLogo />
         </div>
@@ -30,10 +30,10 @@ export default function AboutPage() {
           <TailwindLogo />
         </div>
       </div>
-      <h2 className="w-fit mt-1 text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
+      <h2 className="w-fit mt-3.5 sm:mt-1 text-2xl md:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-400">
         Credits
       </h2>
-      <ul className="p-0.5 md:p-1 text-sm text-stone-300">
+      <ul className="max-sm:mt-0.5 p-0.5 md:p-1 text-sm text-stone-300">
         <li className="max-sm:mb-1 mb-0.5">
           Icons for programming tools and languages from{" "}
           <a
