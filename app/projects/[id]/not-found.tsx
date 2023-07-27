02a4a2ec6f427e2a4c36c4dade2bd357cd99e278
Viktor <vikt.mag@gmail.com>
2023-07-27 23:42:43 +0200

    Changed styling of project not-found page

--- app/projects/[id]/not-found.tsx
+++ app/projects/[id]/not-found.tsx
@@ -4,11 +4,11 @@ export default function NotFound() {
   // Display this page if user types in Price Scrape ID that does not exist
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="text-neutral-100 font-extrabold text-2xl md:text-6xl pb-5">
+      <h1 className="text-neutral-100 font-extrabold text-2xl md:text-6xl pb-3 md:pb-5">
         Project Does Not Exist!
       </h1>
       <Link href="/#projects">
-        <button className="mb-5 py-2.5 px-4 rounded-xl font-bold text-md md:text-lg text-zinc-100 bg-neutral-800 hover:bg-neutral-700 border-2 border-neutral-100">
+        <button className="not-found-page-back-button py-1.5 px-3.5 md:py-2.5 md:px-4 rounded-xl font-bold text-base md:text-xl text-zinc-100 bg-neutral-800 hover:bg-stone-700 border-2 border-neutral-100">
           Back To Projects
         </button>
       </Link>
