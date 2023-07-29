d18eec333f80e21a04e3a39d4384a5344b6902f7
Viktor <vikt.mag@gmail.com>
2023-07-29 23:40:07 +0200

    Added title to About page

--- app/@modal/(.)about/page.tsx
+++ app/@modal/(.)about/page.tsx
@@ -2,9 +2,14 @@ import AboutPage from "@/app/components/AboutPage";
 import Modal from "@/app/components/Modal";
 
 export default function AboutModal() {
-    return <Modal>
-    <div className="max-md:text-center md:flex md:flex-row md:justify-between">
-      <AboutPage />
-    </div>
-  </Modal>
+  return (
+    <>
+    <title>About</title>
+      <Modal>
+        <div className="max-md:text-center md:flex md:flex-row md:justify-between">
+          <AboutPage />
+        </div>
+      </Modal>
+    </>
+  );
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,22 +4,25 @@ import AboutPage from "../components/AboutPage";
 
 export default function About() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div>
-        <Link href="/">
-          <button className="single-page-back-button mb-5 py-2.5 px-4 rounded-xl font-bold text-xl text-stone-100 hover:bg-stone-700">
-            <span className="inline-block align-middle">
-              <BackArrow />
-            </span>
-            <span className="inline-block align-middle ml-2.5">
-              Back To Homepage
-            </span>
-          </button>
-        </Link>
-        <div className="px-4 py-3 md:py-4 rounded-xl max-md:text-center project-page-container">
-          <AboutPage />
+    <>
+      <title>About</title>
+      <div className="flex h-screen justify-center items-center">
+        <div>
+          <Link href="/">
+            <button className="single-page-back-button mb-5 py-2.5 px-4 rounded-xl font-bold text-xl text-stone-100 hover:bg-stone-700">
+              <span className="inline-block align-middle">
+                <BackArrow />
+              </span>
+              <span className="inline-block align-middle ml-2.5">
+                Back To Homepage
+              </span>
+            </button>
+          </Link>
+          <div className="px-4 py-3 md:py-4 rounded-xl max-md:text-center project-page-container">
+            <AboutPage />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
