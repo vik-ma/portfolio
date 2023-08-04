9657bb89e60f6255b55cf175fcd8643938a3786f
Viktor <vikt.mag@gmail.com>
2023-08-04 23:58:50 +0200

    Made About page height consistent with Projects page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,8 @@ export default function About() {
   return (
     <>
       <title>About</title>
-      <div className="flex h-screen justify-center items-center">
-        <div>
+      <div className="flex justify-center items-center min-[871px]:h-screen py-6">
+        <div className="m-auto">
           <Link href="/">
             <button className="single-page-back-button mb-4 py-2.5 px-4 rounded-xl font-bold text-xl text-stone-100 hover:bg-stone-700">
               <span className="inline-block align-middle">
