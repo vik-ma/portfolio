9b52bf9c566cf85b10b78fc7199414a911846e00
Viktor <vikt.mag@gmail.com>
2023-08-09 11:11:24 +0200

    Added short intro to HeroSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,14 +3,19 @@ import Link from "next/link";
 export default function HeroSection() {
   return (
     <div className="rounded-3xl mt-1.5 min-[491px]:mt-3 sm:mt-5 md:mt-6 min-[871px]:mt-10 rounded-3xl bg-gradient-to-l from-blue-500 via-pink-600 to-yellow-400 p-1.5">
-      <div className="hero-container h-[200px] text-center py-2 md:py-3 font-sans">
-        <div className="flex flex-col justify-between py-3 h-full">
-          <div className="flex flex-col justify-center font-semibold text-stone-100 text-3xl">
+      <div className="hero-container h-[200px] text-center p-2 md:p-3 font-sans">
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex flex-col justify-around h-full px-5 font-semibold text-stone-100 text-2xl">
+            <p className="text-4xl font-bold">Welcome</p>
             <p className="">
-              Introduction
+              I&apos;m{" "}
+              <span className="font-bold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 to-orange-400">
+                Viktor Magnusson
+              </span>
+              , a full-stack developer from Sweden.
             </p>
           </div>
-          <div className="flex flex-row justify-evenly text-stone-100">
+          {/* <div className="flex flex-row justify-evenly text-stone-100">
             <Link
               className="font-bold text-xl md:text-3xl"
               href={"/#languages"}
@@ -23,7 +28,7 @@ export default function HeroSection() {
             <Link className="font-bold text-xl md:text-3xl" href={"/about"}>
               About
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
