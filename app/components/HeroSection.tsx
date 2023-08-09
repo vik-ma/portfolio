addf923fe1c9cb26d3f489def9f8c2a138320138
Viktor <vikt.mag@gmail.com>
2023-08-09 05:16:20 +0200

    Changed basic styling for HeroSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,11 +3,14 @@ import Link from "next/link";
 export default function HeroSection() {
   return (
     <div className="rounded-3xl mt-1.5 min-[491px]:mt-3 sm:mt-5 md:mt-6 min-[871px]:mt-10 rounded-3xl bg-gradient-to-l from-blue-500 via-pink-600 to-yellow-400 p-1.5">
-      <div className="hero-container h-[200px] text-center py-2 md:py-3 font-sans text-sm text-zinc-100">
-        <div className="flex flex-col justify-between p-2 h-full">
-          <div className="flex justify-center mt-10">
+      <div className="hero-container h-[200px] text-center py-2 md:py-3 font-sans">
+        <div className="flex flex-col justify-between py-3 h-full">
+          <div className="flex flex-col justify-center font-semibold text-zinc-100 text-3xl">
+            <p className="">
+              Introduction
+            </p>
           </div>
-          <div className="flex flex-row justify-evenly">
+          <div className="flex flex-row justify-evenly text-zinc-100">
             <Link
               className="font-bold text-xl md:text-3xl"
               href={"/#languages"}
