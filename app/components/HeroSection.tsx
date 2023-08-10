4f79936c340f95d7a5f94bef68b32c9875037a3e
Viktor <vikt.mag@gmail.com>
2023-08-10 22:02:18 +0200

    Removed the margin-rights for links in HeroSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -22,10 +22,10 @@ export default function HeroSection() {
               of my programming projects and the technologies I used to develop
               them. Click on the projects to learn more about them!
             </p>
-            <div className="flex flex-row justify-center gap-x-3 text-stone-100">
-              <div className="flex flex-row gap-x-3">
+            <div className="flex flex-row justify-center gap-x-5 text-stone-100">
+              <div className="flex flex-row gap-x-5">
                 <Link
-                  className="flex w-fit whitespace-nowrap mr-1.5 py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
+                  className="flex w-fit whitespace-nowrap py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
                   href={"/#languages"}
                 >
                   <span>
@@ -34,7 +34,7 @@ export default function HeroSection() {
                   <span className="ml-1.5">Skills</span>
                 </Link>
                 <Link
-                  className="flex w-fit whitespace-nowrap mr-1.5 py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
+                  className="flex w-fit whitespace-nowrap py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
                   href={"/#projects"}
                 >
                   <span>
@@ -43,9 +43,9 @@ export default function HeroSection() {
                   <span className="ml-1.5">Projects</span>
                 </Link>
               </div>
-              <div className="flex flex-row gap-x-3">
+              <div className="flex flex-row gap-x-5">
                 <Link
-                  className="flex w-fit whitespace-nowrap mr-1.5 py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
+                  className="flex w-fit whitespace-nowrap py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
                   href={"/about"}
                 >
                   <span>
@@ -56,7 +56,7 @@ export default function HeroSection() {
                 <a
                   href="https://github.com/vik-ma"
                   target="_blank"
-                  className="flex w-fit whitespace-nowrap mr-1.5 py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
+                  className="flex w-fit whitespace-nowrap py-1 pl-1.5 pr-2 text-base font-bold rounded-[10px] cursor-pointer border-2 border-stone-500 text-stone-100 bg-stone-600/20 hover:text-white hover:bg-stone-400/30 transition ease-out duration-200"
                 >
                   <span>
                     <GitHubLogo />
