8f00a0ecfec811ee22fd80c6128c0dda0c1355e8
Viktor <vikt.mag@gmail.com>
2023-08-10 22:40:42 +0200

    Tweaked the paddings between sections on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import HeaderAnimation from "./components/HeaderAnimation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-1 md:pt-7 pb-7">
-      <div className="relative header-wrapper flex flex-row justify-center pb-4 md:pb-8">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-1 md:pt-7 pb-6">
+      <div className="relative header-wrapper flex flex-row justify-center pb-4 md:pb-7">
         <HeaderAnimation />
       </div>
-      <header className="pb-10 md:pb-16">
+      <header className="pb-10 md:pb-12">
         <HeroSection />
       </header>
-      <section className="pt-1 sm:pt-1.5 pb-10 md:pb-16" id="languages">
+      <section className="pt-1 sm:pt-1.5 pb-10 md:pb-12" id="languages">
         <LanguagesSection />
       </section>
       <section className="pt-1 sm:pt-1.5" id="projects">
