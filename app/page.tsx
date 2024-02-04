820efa869f621097b8f7fa9a35d7a5a74f8e5e45
Viktor <vikt.mag@gmail.com>
2024-02-04 22:11:23 +0100

    Tweak spacing of items on home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import HeaderAnimation from "./components/HeaderAnimation";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-1 md:pt-7 pb-6">
-      <div className="relative header-wrapper flex flex-row justify-center pb-4 md:pb-7">
+      <div className="relative header-wrapper flex flex-row justify-center pb-4 md:pb-6">
         <HeaderAnimation />
       </div>
-      <header className="pb-10 md:pb-14">
+      <header className="pb-8 md:pb-12">
         <HeroSection />
       </header>
-      <section className="pt-1 sm:pt-1.5 pb-10 md:pb-14" id="languages">
+      <section className="pt-1 sm:pt-1.5 pb-8 md:pb-12" id="languages">
         <LanguagesSection />
       </section>
       <section className="pt-1 sm:pt-1.5" id="projects">
