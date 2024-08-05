d36d1c4a22cba7b7aa8328edcf32cbb66c846aac
Viktor <vikt.mag@gmail.com>
2024-08-05 10:48:11 +0200

    Tweak font-size for links in ProjectPage on smaller screens

--- app/components/ProjectPage.tsx
+++ app/components/ProjectPage.tsx
@@ -22,24 +22,24 @@ export default function ProjectPage({ project }: ProjectProps) {
                 <a
                   href={project?.githubLink}
                   target="_blank"
-                  className="flex w-fit whitespace-nowrap mr-1.5 py-1 pl-1.5 pr-2 font-bold rounded-lg cursor-pointer border-2 border-lime-300 text-stone-50 bg-lime-400/10 hover:text-white hover:bg-lime-400/40 transition ease-out duration-200"
+                  className="flex w-fit items-center whitespace-nowrap mr-1.5 py-1 pl-1.5 pr-2 font-bold rounded-lg cursor-pointer border-2 border-lime-300 text-stone-50 bg-lime-400/10 hover:text-white hover:bg-lime-400/40 transition ease-out duration-200"
                 >
                   <span>
                     <GitHubLogo />
                   </span>
-                  <span className="ml-1.5">GitHub Repository</span>
+                  <span className="ml-1.5 text-sm sm:text-base">GitHub Repository</span>
                 </a>
                 {project?.liveSiteLink !== "" && (
                   // Display a button that links to the project's website if a link exists
                   <a
                     href={project?.liveSiteLink}
                     target="_blank"
-                    className="flex w-fit whitespace-nowrap py-1 pl-1.5 pr-2 font-bold rounded-lg cursor-pointer border-2 border-red-500 text-stone-50 bg-red-500/10 hover:text-white hover:bg-red-500/40 transition ease-out duration-200"
+                    className="flex w-fit items-center whitespace-nowrap py-1 pl-1.5 pr-2 font-bold rounded-lg cursor-pointer border-2 border-red-500 text-stone-50 bg-red-500/10 hover:text-white hover:bg-red-500/40 transition ease-out duration-200"
                   >
                     <span>
                       <LiveSite />
                     </span>
-                    <span className="ml-1.5">Live Site</span>
+                    <span className="ml-1.5 text-sm sm:text-base">Live Site</span>
                   </a>
                 )}
               </div>
